59d64fe9b69f1a13dd1f2c88dfed342c9d7a4665
vin-ivar <vinit.ravishankar@gmail.com>
2018-04-05 23:48:14 +0200

    super legit, i should have started with this

--- Loader.py
+++ Loader.py
@@ -1,3 +1,4 @@
+import os
 import codecs
 from torchtext import data, datasets
 
@@ -29,3 +30,36 @@ def conll_to_csv(fname):
 
 if __name__ == '__main__':
     a = conll_to_csv('data/sv-ud-train.conllu')
+    if not os.path.exists(".tmp"):
+        os.makedirs(".tmp")
+
+    with open(os.path.join(".tmp", "train.csv"), "w") as f:
+        f.write(a)
+
+    tokeniser = lambda x: x.split(',')
+    tokint = lambda x: list(map(lambda y: int(y), x.split(',')))
+    ID = data.Field(tokenize=tokeniser, batch_first=True)
+    FORM = data.Field(tokenize=tokeniser, batch_first=True, include_lengths=True)
+    LEMMA = data.Field(tokenize=tokeniser, batch_first=True)
+    UPOS = data.Field(tokenize=tokeniser, batch_first=True)
+    XPOS = data.Field(tokenize=tokeniser, batch_first=True)
+    FEATS = data.Field(tokenize=tokeniser, batch_first=True)
+    HEAD = data.Field(tokenize=tokeniser, batch_first=True)
+    DEPREL = data.Field(tokenize=tokeniser, batch_first=True)
+    DEPS = data.Field(tokenize=tokeniser, batch_first=True)
+    MISC = data.Field(tokenize=tokeniser, batch_first=True)
+    fields = [ID, FORM, LEMMA, UPOS, XPOS, FEATS, HEAD, DEPREL, DEPS, MISC]
+
+
+    train = data.TabularDataset(path=".tmp/train.csv", format="csv", fields=[('id', ID), ('form', FORM),
+                                                                             ('lemma', LEMMA), ('upos', UPOS),
+                                                                             ('xpos', XPOS), ('feats', FEATS),
+                                                                             ('head', HEAD), ('deprel', DEPREL),
+                                                                             ('deps', DEPS), ('misc', MISC)])
+
+    [i.build_vocab(train) for i in fields]
+    (train_iter,) = data.Iterator.splits((train,), batch_sizes=(32,), device=-1)
+    for i, batch in enumerate(train_iter):
+        (forms, sizes), tags, heads, deprels = batch.form, batch.upos, batch.head, batch.deprel
+
+
