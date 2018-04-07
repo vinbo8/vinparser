d971bac0af3b74ceb77b10365f238b5fd7eb17cf
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-04-07 14:02:42 +0200

    minor loader changes

--- Loader.py
+++ Loader.py
@@ -4,12 +4,12 @@ import numpy as np
 from torchtext import data, datasets
 
 
-ROOT_LINE = "0\t__ROOT\t_\t__ROOT\t_\t_\t0\t__ROOT\t_\t_"
+ROOT_LINE = "0\t__ROOT\t_\t_\t__ROOT\t_\t_\t0\t__ROOT\t_\t_"
 
 
 def conll_to_csv(fname):
     with codecs.open(fname, 'r', 'utf-8') as f:
-        rows, blokk = [], ['"' for _ in range(10)]
+        rows, blokk = [], ['"' for _ in range(11)]
         blokk = list(map(lambda x, y: x + y, blokk, ROOT_LINE.split("\t")))
         for line in f:
             if line[0] == '#':
@@ -19,11 +19,12 @@ def conll_to_csv(fname):
                 blokk = list(map(lambda x: x + '"', blokk))
                 blokk = ",".join(blokk)
                 rows.append(blokk)
-                blokk = ['"' for _ in range(10)]
+                blokk = ['"' for _ in range(11)]
                 blokk = list(map(lambda x, y: x + y, blokk, ROOT_LINE.split("\t")))
                 continue
 
             cols = [i.replace(',', '<cm>') for i in line.rstrip("\n").split("\t")]
+            cols = cols[:2] + [cols[1]] + cols[2:]
             blokk = list(map(lambda x, y: x + ',' + y, blokk, cols))
 
     return "\n".join(rows)
@@ -49,6 +50,8 @@ def get_iterators(args, batch_size):
 
     tokeniser = lambda x: x.split(',')
     ID = data.Field(tokenize=tokeniser, batch_first=True)
+    FORM_TO_CHAR = data.Field(tokenize=tokeniser, batch_first=True)
+    CHAR = data.NestedField(FORM_TO_CHAR, tokenize=list)
     FORM = data.Field(tokenize=tokeniser, batch_first=True, include_lengths=True)
     LEMMA = data.Field(tokenize=tokeniser, batch_first=True)
     UPOS = data.Field(tokenize=tokeniser, batch_first=True)
@@ -60,13 +63,13 @@ def get_iterators(args, batch_size):
     MISC = data.Field(tokenize=tokeniser, batch_first=True)
 
     train, dev, test = data.TabularDataset.splits(path=".tmp", train='train.csv', validation='dev.csv', test='test.csv',
-                                                  format="csv", fields=[('id', ID), ('form', FORM),
+                                                  format="csv", fields=[('id', ID), ('form', FORM), ('char', CHAR),
                                                                         ('lemma', LEMMA), ('upos', UPOS),
                                                                         ('xpos', XPOS), ('feats', FEATS),
                                                                         ('head', HEAD), ('deprel', DEPREL),
                                                                         ('deps', DEPS), ('misc', MISC)])
 
-    fields = [ID, FORM, LEMMA, UPOS, XPOS, FEATS, HEAD, DEPREL, DEPS, MISC]
+    fields = [ID, FORM, CHAR, LEMMA, UPOS, XPOS, FEATS, HEAD, DEPREL, DEPS, MISC]
     [i.build_vocab(train) for i in fields]
 
     (train_iter, dev_iter, test_iter) = data.Iterator.splits((train, dev, test), batch_sizes=(batch_size, batch_size, batch_size), device=device,
