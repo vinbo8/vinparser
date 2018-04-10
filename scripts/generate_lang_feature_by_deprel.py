bb5f713759ffcf687c0ddf7e81b65ddbfb942c5a
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-04-11 00:51:30 +0200

    started adding seeds - todo - eval this stuffs

---
+++ scripts/generate_lang_feature_by_deprel.py
@@ -0,0 +1,40 @@
+import random
+import sys
+
+random.seed(1337)
+
+def do_stuff(f):
+	toks = []
+	for line in f:
+		if line[0] == '#' or not line.strip():
+			continue
+
+		cols = line.split("\t")
+		if cols[7] == 'case':
+			toks.append(cols[1])
+
+	return toks
+
+with open(sys.argv[1]) as f:
+	l1 = f.readlines()
+
+with open(sys.argv[2]) as f:
+	l2 = f.readlines()
+
+with open(sys.argv[3]) as f:
+	combo = f.readlines()
+
+# build samples
+toks = do_stuff(l1) + do_stuff(l2)
+
+for line in combo:
+	if line[0] == '#' or not line.strip():
+		sys.stdout.write(line)
+		continue
+
+	cols = line.split("\t")
+	if cols[7] == 'case':
+		sys.stdout.write("{}\tLanguage={}".format("\t".join(cols[0:9]), random.choice(['en', 'hi'])))
+	else:
+		sys.stdout.write(line)
+
