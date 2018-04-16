50f5e9f1e62ab042e248d05f7057497d8390584f
mhany90 <s3094723@peregrine.hpc.rug.nl>
2018-04-16 22:50:11 +0200

    for merge

--- Loader_sem.py
+++ Loader_sem.py
@@ -14,7 +14,7 @@ ROOT_LINE = "0\t__ROOT\t_\t_\t__ROOT\t_\t_\t0\t__ROOT\t_\t_t_"
 
 def conll_to_csv(fname):
     with codecs.open(fname, 'r', 'utf-8') as f:
-        rows, blokk = [], ['"' for _ in range(11)]
+        rows, blokk = [], ['"' for _ in range(12)]
         blokk = list(map(lambda x, y: x + y, blokk, ROOT_LINE.split("\t")))
         for line in f:
             if line[0] == '#':
