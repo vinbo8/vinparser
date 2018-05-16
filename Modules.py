3811a0333eac2e75de1ac2c856ac3b27f493eab4
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-05-16 19:37:02 +0200

    sizes

--- Modules.py
+++ Modules.py
@@ -7,12 +7,12 @@ import torch.nn.functional as F
 class CharEmbedding(torch.nn.Module):
     def __init__(self, char_size, embed_dim, lstm_dim, lstm_layers):
         super().__init__()
-        self.embedding_chars = torch.nn.Embedding(char_size, embed_dim)
-        self.lstm = torch.nn.LSTM(embed_dim, int(150), lstm_layers,
+        self.embedding_chars = torch.nn.Embedding(char_size, 25)
+        self.lstm = torch.nn.LSTM(25, int(150), lstm_layers,
                                   batch_first=True, bidirectional=False, dropout=0.33)
         self.attention = LinearAttention(int(150))
-        self.mlp = torch.nn.Linear(300, embed_dim, bias=False)
-        self.lstm = torch.nn.LSTM(embed_dim, lstm_dim, lstm_layers,
+        self.mlp = torch.nn.Linear(300, 25, bias=False)
+        self.lstm = torch.nn.LSTM(25, lstm_dim, lstm_layers,
                                   batch_first=True, bidirectional=False, dropout=0.33)
         self.attention = LinearAttention(lstm_dim)
         self.mlp = torch.nn.Linear(2 * lstm_dim, embed_dim, bias=False)
--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -5,9 +5,9 @@ if [[ -f "$root/results/$1" ]]; then echo "$1 exists: removing.. "; rm "$root/re
 cd ~/personal_work_troja/vinparser
 git checkout master
 git pull
-echo "$root/venv/bin/python $root/Runner.py --parse --use_cuda --save $root/models/en.pt \
+echo "$root/venv/bin/python $root/Runner.py --parse --use_cuda --use_chars --save $root/models/en_baseline.pt \
 --train $root/data/en-ud-train.conllu.sem \
 --dev $root/data/en-ud-dev.conllu.sem --test $root/data/en-ud-test.conllu.sem \
---config $root/config.ini" > parse.sh
+--embed $root/data/embeds/wiki.en.vec --config $root/config.ini" > parse.sh
 if [ ! -d "results" ]; then mkdir results; fi
 qsub -e "$root/results/err" -o "$root/results/$1" -q gpu.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=2G parse.sh
