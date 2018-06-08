927d3e04a65979e345e4f5e1350f17e92792eedd
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-06-08 20:13:31 +0200

    fixed conll

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -4,9 +4,10 @@ cd $root
 git checkout eval_codeswitch
 git pull
 echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda \
---train $root/thesis/data/en-hi/en-hi-langid.conllu \
+--train $root/thesis/data/en-hi/en-hi-append.conllu \
 --dev $root/thesis/data/en-hi/en-hi-dev.conllu \
 --test $root/thesis/data/en-hi/en-hi-test.conllu \
 --save $root/thesis/models/$1.vin \
+--embed $root/thesis/embeds/en-hi-append.vec \
 --config $root/config.ini" > parse.sh
 qsub -e "$root/thesis/results/err" -o "$root/thesis/results/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=6G parse.sh
