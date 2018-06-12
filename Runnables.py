9168463844a46af0c894513f49410b013854e14c
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-06-12 18:34:33 +0200

    weird thing

--- Runnables.py
+++ Runnables.py
@@ -768,9 +768,9 @@ class LangSwitch(torch.nn.Module):
         self.chain = chain
         if self.args.embed:
             self.embeds.weight.data.copy_(vocab[0].vectors)
-        self.lstm = torch.nn.LSTM(3 * embed_dim, lstm_dim, lstm_layers, batch_first=True, bidirectional=True, dropout=0.5)
+        self.lstm = torch.nn.LSTM(1 * embed_dim, lstm_dim, lstm_layers, batch_first=True, bidirectional=True, dropout=0.5)
         self.relu = torch.nn.ReLU()
-        self.mlp = torch.nn.Linear(2 * lstm_dim, mlp_dim)
+        self.mlp = torch.nn.Linear(lstm_dim + 2 * embed_dim, mlp_dim)
         self.out = torch.nn.Linear(mlp_dim, sizes['misc'])
         self.criterion = torch.nn.CrossEntropyLoss(ignore_index=-1)
         self.optimiser = torch.optim.Adam(self.parameters(), lr=learning_rate, betas=(0.9, 0.9))
@@ -785,13 +785,14 @@ class LangSwitch(torch.nn.Module):
         deprel_embeds = self.dropout(self.deprel_embeds(deprels))
         previous_langid = self.dropout(self.previous_langid_embeds(misc))
 
-        embeds = torch.cat([form_embeds, tag_embeds, previous_langid], dim=2)
+        # embeds = torch.cat([form_embeds, tag_embeds, previous_langid], dim=2)
 
-        packed = torch.nn.utils.rnn.pack_padded_sequence(embeds, pack.tolist(), batch_first=True)
+        packed = torch.nn.utils.rnn.pack_padded_sequence(previous_langid, pack.tolist(), batch_first=True)
         lstm_out, _ = self.lstm(packed)
         lstm_out, _ = torch.nn.utils.rnn.pad_packed_sequence(lstm_out, batch_first=True)
 
-        mlp_out = self.dropout(self.relu(self.mlp(lstm_out)))
+        for_mlp = torch.cat([form_embeds, tag_embeds, lstm_out], dim=2)
+        mlp_out = self.dropout(self.relu(self.mlp(for_mlp)))
 
         y_pred = self.out(mlp_out)
         if self.args.use_cuda:
