daa3bd1798a3521061212446b8b35b717d96172d
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-07-15 19:35:59 +0200

    job

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -2,13 +2,17 @@
 root="/home/ravishankar/personal_work_troja/vinparser"
 cd $root
 git checkout eval_codeswitch_domain
+mkdir -p "$root/thesis/conlls/domain"
+mkdir -p "$root/thesis/models/domain"
 git pull
+
 echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda \
 --embed $root/thesis/embeds/en-hi-mapped.vec \
 --train $root/thesis/data/en-hi/en-hi-train-domain.conllu \
 --dev $root/thesis/data/en-hi/en-hi-test-domain.conllu \
 --test $root/thesis/data/en-hi/en-hi-test-domain.conllu \
---save $root/thesis/models/$1.vin \
---outfile $root/thesis/conlls/$1.conllu \
+--save $root/thesis/models/domain/$1.vin \
+--outfile $root/thesis/conlls/domain/$1.conllu \
 --config $root/config.ini" > parse.sh
-qsub -e "$root/thesis/results/err" -o "$root/thesis/results/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=10G parse.sh
+
+qsub -e "$root/thesis/results/err_$1" -o "$root/thesis/results/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=10G parse.sh
