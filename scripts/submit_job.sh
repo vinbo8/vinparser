f8e2f19fc821c31884427ae3736d544832e6bb4a
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-07-15 19:47:51 +0200

    switching

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -5,7 +5,7 @@ git checkout eval_codeswitch
 git pull
 echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda \
 --embed $root/thesis/embeds/en-hi-mapped.vec \
---train $root/thesis/data/en-hi/en-hi-append.conllu \
+--train $root/thesis/data/en-hi/en-hi-domain.conllu \
 --dev $root/thesis/data/en-hi/en-hi-dev.conllu \
 --test $root/thesis/data/en-hi/en-hi-test.conllu \
 --save $root/thesis/models/$1.vin \
