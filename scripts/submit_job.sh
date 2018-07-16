c67845b0999fcd4c8cd9fb4ddff9ecace6b5d504
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-07-17 01:19:02 +0200

    switching

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -14,4 +14,4 @@ echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda \
 --save $root/thesis/models/$1.vin \
 --outfile $root/thesis/conlls/langids/$1.conllu \
 --config $root/config.ini" > parse.sh
-qsub -e "$root/thesis/results/err_$1" -o "$root/thesis/results/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=4G -p 0 parse.sh
+qsub -e "$root/thesis/results/err_$1" -o "$root/thesis/results/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=10G -p 0 parse.sh
