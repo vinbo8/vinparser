b0bc6609f5509aad497d628b4b22a853587a4c1d
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-07-16 11:49:08 +0200

    better submitter

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -3,12 +3,15 @@ root="/home/ravishankar/personal_work_troja/vinparser"
 cd $root
 git checkout eval_codeswitch
 git pull
+
+mkdir -p "$root/thesis/conlls/baselines"
+
 echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda \
 --embed $root/thesis/embeds/en-hi-mapped.vec \
 --train $root/thesis/data/en-hi/en-hi-domain.conllu \
 --dev $root/thesis/data/en-hi/en-hi-dev.conllu \
 --test $root/thesis/data/en-hi/en-hi-test.conllu \
 --save $root/thesis/models/$1.vin \
---outfile $root/thesis/conlls/$1.conllu \
+--outfile $root/thesis/conlls/baselines/$1.conllu \
 --config $root/config.ini" > parse.sh
-qsub -e "$root/thesis/results/err" -o "$root/thesis/results/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=10G parse.sh
+qsub -e "$root/thesis/results/err_$1" -o "$root/thesis/results/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=10G parse.sh
