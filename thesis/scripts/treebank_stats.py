f2f2765dd13b96a4b1f2c186a297fa15856fed5e
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-07-16 11:47:25 +0200

    stats

--- thesis/scripts/treebank_stats.py
+++ thesis/scripts/treebank_stats.py
@@ -6,35 +6,37 @@ import numpy as np
 blokk = []
 deprels_to_count = []
 
-m_index = 0
-# i-index
-number_of_sentences = 0
-current_lang = None
-switches = 0
-total_i_index = 0
-# burstiness
-previous_lang = None
-current_sample = 0
-samples = []
-init_vector = []
-final_vector = []
+stats = {'m': 0, 'i': 0, 'burst': 0, 'le': 0, 'se': 0, 'mem': 0}
+total_sentences = 0
 
 for line in sys.stdin:
     if line[0] == '#':
         continue
 
     if not line.rstrip():
+        total_sentences += 1
+
         # m-index
         langs = set()
         tokens_per_lang = {}
-        # ==
-        number_of_sentences += 1
-        current_lang = None
-        current_sample = 0
+
+        # language entropy
+        # nah
+
+        # i-index
+        current_language = None
+        switches = 0
+
+        # time stuff
+        span_current_language = None
+        current_span = 0
+        spans = []
+
         # do stuff here
         for row in blokk:
-            # m-index
             lang = row[9]
+
+            # m-index
             langs.add(lang)
             try:
                 tokens_per_lang[lang] += 1
@@ -43,25 +45,25 @@ for line in sys.stdin:
             # ---
 
             # i-index
-            lang = row[9]
-            if not current_lang:
-                current_lang = lang
-            else:
-                if lang != current_lang:
-                    switches += 1
+            if not current_language:
+                current_language = lang
+            elif current_language != lang:
+                current_language = lang
+                switches += 1
             # ---
-
+            
             # burstiness
-            lang = row[9]
-            if lang != current_lang:
-                samples.append(current_sample)
-                current_sample = 1
-
-            else:
-                current_sample += 1 
+            if not span_current_language:
+                span_current_language = lang
+                current_span += 1
+            elif span_current_language == lang:
+                current_span += 1
+            elif span_current_language != lang:
+                spans.append(current_span)
+                current_span = 1
             # ---
-            current_lang = lang
 
+            # other
             head = int(row[6]) - 1
             lang = row[9]
             if head != -1:
@@ -70,64 +72,51 @@ for line in sys.stdin:
                     deprels_to_count.append(row[7])
 
         # ---
-        # sentence level
+
         # m-index
         k = len(langs)
         total_tokens = sum(tokens_per_lang.values())
         pj_sum = 0
         for current_lang in tokens_per_lang:
             pj_sum += (tokens_per_lang[current_lang] / total_tokens) ** 2
+
+        stats['m'] += (1 - pj_sum) / ((k - 1) * pj_sum)
+
+        # le
+        lang_ent = 0
+        for current_lang in tokens_per_lang:
+            pj = tokens_per_lang[current_lang] / total_tokens
+            lang_ent -= pj * math.log2(pj)
+        
+        stats['le'] += lang_ent
+
         # i-index
-        total_i_index += switches / (len(blokk) - 1)
-        current_lang = None ; switches = 0
-        samples.append(current_sample)
-        # ---
-        blokk = []
+        stats['i'] += switches / (len(blokk) - 1)
+
+        # burstiness
+        mean = np.mean(spans)
+        std = np.std(spans)
+        stats['burst'] += (std - mean) / (std + mean)
+
+        # memory
+        m1 = np.mean(spans[:-1])
+        s1 = np.std(spans[:-1])
+        m2 = np.mean(spans[1:])
+        s2 = np.std(spans[1:])
+
+        # span entropy
+        span_ent = 0
+        span_classes = set(spans)
+        for span in span_classes:
+            p = spans.count(span) / len(spans)
+            span_ent -= p * math.log2(p)
+
+        stats['se'] += span_ent
         continue
 
     cols = line.split("\t")
     blokk.append(cols)
 
 # process collected stats
-
-# m-index
-k = len(langs)
-total_tokens = sum(tokens_per_lang.values())
-pj_sum = 0
-for current_lang in tokens_per_lang:
-    pj_sum += (tokens_per_lang[current_lang] / total_tokens) ** 2
-
-m_index = (1 - pj_sum) / ((k - 1) * pj_sum)
-print("M-index: ", m_index)
-# ---
-
-# language entropy
-LE = 0
-for current_lang in tokens_per_lang:
-    pj = tokens_per_lang[current_lang] / total_tokens
-    LE -= pj * math.log2(pj)
-
-print("LE: ", LE)
-# ---
-
-# i-index
-print("I-Index: ", total_i_index / number_of_sentences)
-# ---
-
-# burstiness
-mean = np.mean(samples)
-std = np.std(samples)
-burstiness = (std - mean) / (std + mean)
-print("Burstiness: ", burstiness)
-# ---
-
-# span entropy
-SE = 0
-spans = set(samples)
-for sample in spans:
-    pr = samples.count(sample) / len(samples)
-    SE -= pr * math.log2(pr)
-print("Span entropy: ", SE)
-# ---
-
-print(Counter(deprels_to_count))
+processed = {k: v / total_sentences for (k, v) in stats.items()}
+print(processed)
