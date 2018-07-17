9205cb4a55bc4c2521376e1db0df18ba649c9b7a
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-07-17 21:40:06 +0200

    back to compress

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -6,7 +6,7 @@ git pull
 
 mkdir -p "$root/thesis/conlls/embeds"
 
-echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda --use_chars \
+echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda \
 --embed $root/thesis/embeds/kpv-ru-unsupervised.vec \
 --train $root/thesis/data/kpv-ru/kpv-ru-train-append.conllu \
 --dev $root/thesis/data/kpv-ru/kpv-ru-test.conllu \
@@ -14,4 +14,4 @@ echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda --use_chars \
 --save $root/thesis/models/$1.vin \
 --outfile $root/thesis/conlls/embeds/$1.conllu \
 --config $root/config.ini" > parse.sh
-qsub -e "$root/thesis/results/err_$1" -o "$root/thesis/results/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=10G -p 0 parse.sh
+qsub -e "$root/thesis/results/err_$1" -o "$root/thesis/results/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=4G -p 0 parse.sh
