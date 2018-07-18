33e7605340e275c812160ed5b1e240bdf203fb8b
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-07-18 14:03:42 +0200

    stffs

--- Loader.py
+++ Loader.py
@@ -100,7 +100,7 @@ def get_iterators(args, batch_size):
     train, dev, test = data.TabularDataset.splits(path=".tmp", train='train_{}.csv'.format(seconds_since_epoch),
                                                     validation='dev_{}.csv'.format(seconds_since_epoch), 
                                                     test='test_{}.csv'.format(seconds_since_epoch),
-                                                    format="csv", fields=field_tuples, random_state=1337)
+                                                    format="csv", fields=field_tuples)
 
     field_names = [i[1] for i in field_tuples]
     for field in field_names:
--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 root="/home/ravishankar/personal_work_troja/vinparser"
 cd $root
-git checkout eval_codeswitch
+git checkout eval_codeswitch_dev_learning
 git pull
+
+mkdir -p "$root/thesis/conlls/dev_learning"
+
 echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda \
 --embed $root/thesis/embeds/en-hi-mapped.vec \
 --train $root/thesis/data/en-hi/en-hi-append.conllu \
 --dev $root/thesis/data/en-hi/en-hi-dev.conllu \
 --test $root/thesis/data/en-hi/en-hi-test.conllu \
 --save $root/thesis/models/$1.vin \
---outfile $root/thesis/conlls/$1.conllu \
+--outfile $root/thesis/conlls/dev_learning/$1.conllu \
 --config $root/config.ini" > parse.sh
-qsub -e "$root/thesis/results/err" -o "$root/thesis/results/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=10G parse.sh
+qsub -e "$root/thesis/results/err_$1" -o "$root/thesis/results/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=6G parse.sh
