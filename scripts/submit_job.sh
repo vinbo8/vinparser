848b7b67c996d324f994506058f073325513105b
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-07-18 13:28:26 +0200

    submitter

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -6,7 +6,7 @@ git pull
 
 mkdir -p "$root/thesis/conlls/embeds"
 
-echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda --chars \
+echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda --use_chars \
 --embed $root/thesis/embeds/en-hi-append.vec \
 --train $root/thesis/data/en-hi/en-hi-train-append.conllu \
 --dev $root/thesis/data/en-hi/en-hi-test.conllu \
