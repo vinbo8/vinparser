b9e3a964e80d297b49132041b529dbd9b672646b
Vinit Ravishankar <vinit.ravishankar@gmail.com>
2018-07-28 18:42:42 +0200

    branch for other stuffs

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 root="/home/ravishankar/personal_work_troja/vinparser"
 cd $root
-git checkout eval_codeswitch
+git checkout parser_experiments
 git pull
 
-mkdir -p "$root/thesis/conlls/embeds"
+mkdir -p "$root/experiments/conlls/embeds"
 
-echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda --use_chars \
---embed $root/thesis/embeds/kpv-ru-unsupervised.vec \
---train $root/thesis/data/kpv-ru/kpv-ru-train-append.conllu \
---dev $root/thesis/data/kpv-ru/kpv-ru-test.conllu \
---test $root/thesis/data/kpv-ru/kpv-ru-test.conllu \
---save $root/thesis/models/$1.vin \
---outfile $root/thesis/conlls/embeds/$1.conllu \
+echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda \
+--train $root/experiments/data/UD_English/en-ud-train.conllu \
+--dev $root/experiments/data/UD_English/en-ud-dev.conllu \
+--test $root/experiments/data/UD_English/en-ud-test.conllu \
+--outfile $root/experiments/conlls/$1.conllu \
 --config $root/config.ini" > parse.sh
-qsub -e "$root/thesis/results/err_$1" -o "$root/thesis/results/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=10G -p 0 parse.sh
+qsub -e "$root/experiments/logs/err_$1" -o "$root/experiments/logs/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=6G -p 0 parse.sh
