fd5ed6ed2c2aff0900d1537f3906a54a51df62cb
Vinit Ravishankar (student <ravishankar@sol1.ufal.hide.ms.mff.cuni.cz>
2018-09-05 11:28:33 +0200

    stuff

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -13,4 +13,4 @@ echo "$root/venv/bin/python -u $root/Runner.py --parse --use_cuda \
 --test $root/experiments/data/UD_English/en-ud-test.conllu \
 --outfile $root/experiments/conlls/$1.conllu \
 --config $root/config.ini" > parse.sh
-qsub -e "$root/experiments/logs/err_$1" -o "$root/experiments/logs/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=6G -p 0 parse.sh
+qsub -e "$root/experiments/logs/err_$1" -o "$root/experiments/logs/$1" -N "$1" -q gpu-ms.q@dll[256] -l gpu=1,gpu_cc_min3.5=1,gpu_ram=10G -p 0 parse.sh
